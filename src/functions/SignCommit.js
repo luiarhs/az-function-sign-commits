00ce64f21f7d115260de43fc64f74b679f1ae36c
Luis <luis@kief.mx>
2024-02-26 00:30:59 -0600

    [add] graphql call

--- src/functions/SignCommit.js
+++ src/functions/SignCommit.js
@@ -1,13 +1,42 @@
 const { app } = require('@azure/functions');
+import { Octokit } from "@octokit/core";
+
+
 
 app.http('SignCommit', {
-    methods: ['GET', 'POST'],
+    methods: ['POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+        context.log(`Http function processed request for url "${request.url}"`)
+
+        // graphql = graphql.defaults({
+        //     headers: {
+        //         authorization: `token ${process.env.GITHUB_TOKEN}`,
+        //     },
+        // });
+        
+        // try {
+        //     const { response } = await graphql(`
+        //       {
+        //         repository(owner: "luiarhs", name: "datalist") {
+        //           issues(last: 3) {
+        //             edges {
+        //               node {
+        //                 title
+        //               }
+        //             }
+        //           }
+        //         }
+        //       }
+        //     `);
+
+        //     context.log(response);
+        // } catch (error) {
+        //     context.log(`Error! Status: ${error.status}. Message: ${error.response.data.message}`)
+        // }
 
         const name = request.query.get('name') || await request.text() || 'world';
 
-        return { body: `Hello, ${name}!` };
+        return { body: response };
     }
 });
