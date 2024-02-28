a9ad477538cea23430d8b937e68f20e771da7f8b
Luis <luis@kief.mx>
2024-02-27 19:45:24 -0600

    [rem] github pat

--- src/functions/CreateCommit.js
+++ src/functions/CreateCommit.js
@@ -1,36 +1,44 @@
 const { app } = require('@azure/functions');
-const { Octokit } = require('@octokit/graphql');
+const { graphql } = require('@octokit/graphql');
 
 app.http('CreateCommit', {
     methods: ['POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`)
-
-        const octokit = new Octokit({
-            auth: `${process.env["GITHUB_TOKEN"]}`,
-            baseUrl: `${process.env["GITHUB_URL"]}`
-        })
         
         try {
-            const { response } = await octokit.graphql(`
+            const octokit = graphql.defaults({
+				headers: {
+					authorization: `token ${process.env.GITHUB_TOKEN}`
+				}
+            });
+            const { repository } = await octokit(`
               {
-                repository(owner: "luiarhs", name: "datalist") {
-                  issues(last: 3) {
-                    edges {
-                      node {
-                        title
-                      }
-                    }
-                  }
-                }
+				repository(owner: "luiarhs", name: "datalist") {
+					issues(last: 3) {
+						edges {
+							node {
+								title
+							}
+						}
+					}
+				}
               }
             `);
 
-            context.log(response);
+            context.log(repository);
         } catch (error) {
+			if (error instanceof GraphqlResponseError) {
+			
+				console.log("Request failed:", error.request); // { query, variables: {}, headers: { authorization: 'token secret123' } }
+				console.log(error.message); // Field 'bioHtml' doesn't exist on type 'User'
+			} else {
+			// handle non-GraphQL error
+			}
             context.log(`Error! Status: ${error.status}. Message: ${error.response.data.message}`)
         }
-        return { status: 200, body: response };
+
+        return { body: repository };
     }
 });
