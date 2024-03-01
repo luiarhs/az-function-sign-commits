55325e0a3d36a30c5ba010ad95dfc4a12e38f122
luiarhs <luiarhs@yandex.com>
2024-02-29 23:31:25 -0600

    [add] minor update

--- src/functions/CreateCommit.js
+++ src/functions/CreateCommit.js
@@ -2,78 +2,79 @@ const { app } = require("@azure/functions");
 const { graphql } = require("@octokit/graphql");
 
 app.http("CreateCommit", {
-  methods: ["POST"],
-  authLevel: "anonymous",
-  handler: async (request, context) => {
-    context.log(`Http function processed request for url "${request.url}"`);
+	methods: ["POST"],
+	authLevel: "anonymous",
+	handler: async (request, context) => {
+		context.log(`Http function processed request for url "${request.url}"`);
 
-    const octokit = graphql.defaults({
-      headers: {
-        authorization: `token ${process.env.GITHUB_TOKEN}`,
-      },
-    });
+		const octokit = graphql.defaults({
+			headers: {
+				authorization: `token ${process.env.GITHUB_TOKEN}`,
+			},
+		});
 
-    try {
-      const owner = "luiarhs";
-      const repo = "github-actions-test";
-      const branch = "main";
-      const message = "[add] new commit";
-      const content64 ="IyBHaXRIdWIgR3JhcGhRTCBBUEkNCg0KIyMgVGVzdCBjb21taXQgdXNpbmcgZ3JhcGhxbCBpbiBhIEF6IEZu";
+		try {
+			const owner = "luiarhs";
+			const repo = "github-actions-test";
+			const branch = "main";
+			const message = "[add] new commit";
+			const content64 =
+				"IyBHaXRIdWIgR3JhcGhRTCBBUEkNCg0KIyMgVGVzdCBjb21taXQgdXNpbmcgZ3JhcGhxbCBpbiBhIEF6IEZu";
 
-      const response = await octokit(`query  Repository {
-              repository(owner: "${owner}", name: "${repo}") {
-                  ref(qualifiedName: "${branch}") {
-                      target {
-                          ... on Commit {
-                              oid
-                          }
-                      }
-                  }
-			  }
-          }`);
+			const response = await octokit(`query  Repository {
+				repository(owner: "${owner}", name: "${repo}") {
+					ref(qualifiedName: "${branch}") {
+						target {
+							... on Commit {
+								oid
+							}
+						}
+					}
+			  	}
+        	}`);
 
-      const headId = response.repository.ref.target.oid;
+			const headId = response.repository.ref.target.oid;
 
-      // Construct a schema, using GraphQL schema language
-      const data = await octokit(`mutation CreateCommitOnBranch {
-              createCommitOnBranch(
-                  input: {
-                      branch: {
-                          branchName: "${branch}",
-                          repositoryNameWithOwner: "${owner}/${repo}"
-                      }
-                      fileChanges: {
-                          additions: [{
-                              path: "README.md",
-                              contents: "${content64}"
-                          }]
-                      }
-                      message: { headline: "${message}" }
-                      expectedHeadOid: "${headId}"
-                  }
-              ) {
-                  clientMutationId
-                  ref {
-                      id
-                      name
-                      prefix
-                  }
-              }
-          }`);
+			// Construct a schema, using GraphQL schema language
+			const data = await octokit(`mutation CreateCommitOnBranch {
+				createCommitOnBranch(
+					input: {
+						branch: {
+						branchName: "${branch}",
+							repositoryNameWithOwner: "${owner}/${repo}"
+						}
+						fileChanges: {
+							additions: [{
+								path: "README.md",
+								contents: "${content64}"
+							}]
+						}
+						message: { headline: "${message}" }
+						expectedHeadOid: "${headId}"
+					}
+				) {
+					clientMutationId
+					ref {
+						id
+						name
+						prefix
+					}
+				}
+        	}`);
 
-      context.log(repository);
-    } catch (error) {
-      if (error instanceof GraphqlResponseError) {
-        console.log("Request failed:", error.request); // { query, variables: {}, headers: { authorization: 'token secret123' } }
-        console.log(error.message); // Field 'bioHtml' doesn't exist on type 'User'
-      } else {
-        // handle non-GraphQL error
-      }
-      context.log(
-        `Error! Status: ${error.status}. Message: ${error.response.data.message}`
-      );
-    }
+			context.log(repository);
+		} catch (error) {
+			if (error instanceof GraphqlResponseError) {
+				console.log("Request failed:", error.request); // { query, variables: {}, headers: { authorization: 'token secret123' } }
+				console.log(error.message); // Field 'bioHtml' doesn't exist on type 'User'
+			} else {
+				// handle non-GraphQL error
+			}
+			context.log(
+				`Error! Status: ${error.status}. Message: ${error.response.data.message}`
+			);
+		}
 
-    return { body: repository };
-  },
+		return { body: repository };
+	},
 });
